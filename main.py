afddb61e33bed60220072f519015bcae30f14560
Yzzpuri13 <83353956+Yzzpuri13@users.noreply.github.com>
2024-06-16 18:29:32 -0400

    half done

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import random
+
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while(True):
@@ -18,21 +22,44 @@ def deposit():
 def number_of_lines():
     while(True):
         lines = input("Enter the amount of lines to be bet on (1-" + str(MAX_LINES) + ")?  ")
-        if amount.isdigit(): 
+        if lines.isdigit(): 
         # // isdigit tells us if the number entered by user is a valid number or not 
-            lines = int(lines)
-            if lines > 0 :
+            lines = int(lines) 
+            if lines >= 1  and lines <= MAX_LINES :
                 # print(f'amount deposited is {lines}' )
                 break
             else:
-                print("not valid deposit. Please deposit more than 0")
+                print("enter valid number of lines")
         else:
             print("Please Enter an valid Amount")
-    return amount
+    return lines
+
+def get_bet():
+    while(True):
+        bet = input('Enter the amount of the bet : ')
+        if bet.isdigit:
+            bet = int(bet)
+            if MIN_BET <= bet <= MAX_BET:
+                break
+            else:
+                print("Enter bet in range 1 - 100")
+        else:
+            print("Enter a valid digit")
+    return bet
 
 
 def main():
     balance = deposit()
+    lines = number_of_lines()
+    while(True):
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You dont have enough to make this bet your current balance is ${balance}")
+        else:
+            break
+    print(f"you are betting ${bet} on {lines} number of lines and your total bet is ${total_bet}")
 
 main()  
 
